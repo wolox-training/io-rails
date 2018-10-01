3c539f3d3ab4e6e710b07fac106ae9cf5e0eb96f
Ignacio <ignacio.oliveto@wolox.com>
2018-10-01 15:48:15 -0300

    Rubocop Offences corrected

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,10 +4,9 @@ class BooksController < ApplicationController
   def index
     render_paginated Book.all
   end
+
   def shows
     render json: Book.find(params[:id])
   end
   before_action :authenticate_user!
-
-
 end
