c921713656414ef13dc5854fbfc6a43f1703b2eb
Ignacio <ignacio.oliveto@wolox.com.ar>
2018-10-03 15:14:44 -0300

    Fixes requested () added

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -1,65 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Book, type: :model do
-  subject(:book) do
-    Book.new(
-      genre: genre, author: author, image: image, title: title,
-      editor: editor, year: year
-    )
-  end
-
-  let(:genre)            { Faker::Book.genre }
-  let(:author)           { Faker::Book.author }
-  let(:title)            { Faker::Book.title }
-  let(:editor)           { Faker::Book.publisher }
-  let(:image)            { Faker::Name.last_name }
-  let(:year)             { Faker::Number.number(4) }
+  let(:book) { create(:book) }
 
   describe '#create' do
-    context 'When the genre is nil' do
-      let(:genre) { nil }
+    it { should validate_presence_of(:genre) }
 
-      it do
-        is_expected.to be_invalid
-      end
-    end
+    it { should validate_presence_of(:author) }
 
-    context 'When the author is nil' do
-      let(:author) { nil }
+    it { should validate_presence_of(:title) }
 
-      it do
-        is_expected.to be_invalid
-      end
-    end
-    context 'When the image is nil' do
-      let(:image) { nil }
+    it { should validate_presence_of(:editor) }
 
-      it do
-        is_expected.to be_invalid
-      end
-    end
+    it { should validate_presence_of(:image) }
 
-    context 'When the title is nil' do
-      let(:title) { nil }
-
-      it do
-        is_expected.to be_invalid
-      end
-    end
-
-    context 'When the editor is nil' do
-      let(:editor) { nil }
-
-      it do
-        is_expected.to be_invalid
-      end
-    end
-    context 'When the year is nil' do
-      let(:year) { nil }
-
-      it do
-        is_expected.to be_invalid
-      end
-    end
+    it { should validate_presence_of(:year) }
   end
 end
