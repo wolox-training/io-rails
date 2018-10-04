07a07d0492cf8a9d1438b06a3bd9db974ff6e356
Ignacio <ignacio.oliveto@wolox.com.ar>
2018-10-04 11:34:02 -0300

    Rubocop offenses corrected

--- app/controllers/rent_controller.rb
+++ app/controllers/rent_controller.rb
@@ -3,17 +3,17 @@ class RentController < ApplicationController
   include Wor::Paginate
 
   def create
-    rent = Rent.create( user_id: params[:user],
-                  book_id: params[:book],
-                  rent_date: params[:rent_date],
-                  rent_end: params[:rent_end]
-                )
+    rent = Rent.create(user_id: params[:user],
+                       book_id: params[:book],
+                       rent_date: params[:rent_date],
+                       rent_end: params[:rent_end])
     render json: rent
   end
+
   def list
-    user = User.find_by_email!(request.headers["uid"].to_s)
-    render_paginated Rent.where("user_id="+ user.id.to_s)
+    user = User.find_by!(email: request.headers['uid'].to_s)
+    render_paginated Rent.where('user_id=' + user.id.to_s)
   end
 
-  #before_action :authenticate_user!
+  # before_action :authenticate_user!
 end
