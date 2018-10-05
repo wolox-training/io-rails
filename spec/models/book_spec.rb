4ee593b9875a50f408742ca2ab21379906e018ab
Ignacio <ignacio.oliveto@wolox.com.ar>
2018-10-05 16:02:39 -0300

    book_spec style corrected

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -4,16 +4,12 @@ RSpec.describe Book, type: :model do
   let(:book) { create(:book) }
 
   describe '#create' do
-    it { should validate_presence_of(:genre) }
-
-    it { should validate_presence_of(:author) }
-
-    it { should validate_presence_of(:title) }
-
-    it { should validate_presence_of(:editor) }
-
-    it { should validate_presence_of(:image) }
-
-    it { should validate_presence_of(:year) }
+    is_expected_to validate
+    it { is_expected_to validate_presence_of(:genre) }
+    it { is_expected_to validate_presence_of(:author) }
+    it { is_expected_to validate_presence_of(:title) }
+    it { is_expected_to validate_presence_of(:editor) }
+    it { is_expected_to validate_presence_of(:image) }
+    it { is_expected_to validate_presence_of(:year) }
   end
 end
