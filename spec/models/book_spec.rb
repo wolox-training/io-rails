6ab56d1be43f30e92947405edbbfe70d0ce83b6f
Ignacio <ignacio.oliveto@wolox.com.ar>
2018-10-05 16:55:15 -0300

    Style of book_spec corrected

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -3,13 +3,14 @@ require 'rails_helper'
 RSpec.describe Book, type: :model do
   let(:book) { create(:book) }
 
-  describe '#create' do
-    is_expected_to validate
-    it { is_expected_to validate_presence_of(:genre) }
-    it { is_expected_to validate_presence_of(:author) }
-    it { is_expected_to validate_presence_of(:title) }
-    it { is_expected_to validate_presence_of(:editor) }
-    it { is_expected_to validate_presence_of(:image) }
-    it { is_expected_to validate_presence_of(:year) }
-  end
+  it { is_expected.to be_valid }
+
+  it { is_expected_to validate_presence_of(:genre) }
+  it { is_expected_to validate_presence_of(:author) }
+  it { is_expected_to validate_presence_of(:title) }
+  it { is_expected_to validate_presence_of(:editor) }
+  it { is_expected_to validate_presence_of(:image) }
+  it { is_expected_to validate_presence_of(:year) }
+
+
 end
