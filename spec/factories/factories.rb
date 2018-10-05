fbafe43f673d8fb6bcfeb819608920c66a91c585
Ignacio <ignacio.oliveto@wolox.com.ar>
2018-10-05 15:13:06 -0300

    Rucopop Offences corrected

--- spec/factories/factories.rb
+++ spec/factories/factories.rb
@@ -8,9 +8,8 @@ FactoryBot.define do
     year      { Faker::Number.number(4) }
   end
 
-
   factory :user do
-    first_name { Faker::Name.first_name}
-    last_name { Faker::Name.last_name}
+    first_name { Faker::Name.first_name }
+    last_name { Faker::Name.last_name }
   end
 end
