c7739f0dcda9d9ad83f27aec7fc14df233840f2a
Ignacio <ignacio.oliveto@wolox.com.ar>
2018-10-10 16:08:13 -0300

    Pr issues corrected

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,7 @@
 class BooksController < ApplicationController
   include Wor::Paginate
   include DeviseTokenAuth::Concerns::SetUserByToken
+  before_action :authenticate_user!
   def index
     render_paginated Book.all
   end
@@ -8,5 +9,4 @@ class BooksController < ApplicationController
   def shows
     render json: Book.find(params[:id])
   end
-  before_action :authenticate_user!
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
 
   mount_devise_token_auth_for 'User', at: 'auth'
   get 'books', to: 'books#index'
   get 'books/shows' , to: 'books#shows'
-  
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
