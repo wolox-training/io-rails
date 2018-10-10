a837da263ed3123951725d2869d2484d396149af
Ignacio <ignacio.oliveto@wolox.com.ar>
2018-10-10 15:49:22 -0300

    Rubocop Offences corrected

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,7 +5,9 @@ describe User do
 
   describe 'create' do
     it { should validate_presence_of(:first_name) }
-
     it { should validate_presence_of(:last_name) }
+    it { should validate_presence_of(:password) }
+    it { should validate_presence_of(:email) }
+    it { should validate_presence_of(:password) }
   end
 end
