74d5b936b7781ebb39bc3a7a0cb3fc13707fe3fc
Ignacio <ignacio.oliveto@wolox.com.ar>
2018-10-11 14:11:56 -0300

    Duplicated test and active_admin schema deleted

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,6 +8,5 @@ describe User do
     it { should validate_presence_of(:last_name) }
     it { should validate_presence_of(:password) }
     it { should validate_presence_of(:email) }
-    it { should validate_presence_of(:password) }
   end
 end
