867f37c10a692968723b422600e2fa9488a48782
Ignacio <ignacio.oliveto@wolox.com.ar>
2018-10-11 16:46:27 -0300

    Rubocop offenses corrected

--- spec/controllers/books_controller_spec.rb
+++ spec/controllers/books_controller_spec.rb
@@ -2,26 +2,23 @@ require 'rails_helper'
 require 'support/shared_context'
 
 describe BooksController do
-
-
   describe 'GET #index' do
-
     context 'with an authenticated user' do
       include_context 'Authenticated User'
-        context 'When fetching all the books' do
-          let!(:books) { FactoryBot.create_list(:book, 3) }
-            before do
-              get :index
-            end
+      context 'When fetching all the books' do
+        let!(:books) { FactoryBot.create_list(:book, 3) }
+        before do
+          get :index
+        end
 
-          it 'responses with the books json' do
-            expected = ActiveModel::Serializer::CollectionSerializer.new(
+        it 'responses with the books json' do
+          expected = ActiveModel::Serializer::CollectionSerializer.new(
             books, each_serializer: BookSerializer
-            ).to_json
-            expect(response.body.to_json) =~ JSON.parse(expected)
-          end
-          it 'responds with 200 status' do
-            expect(response).to have_http_status(:ok)
+          ).to_json
+          expect(response.body.to_json) =~ JSON.parse(expected)
+        end
+        it 'responds with 200 status' do
+          expect(response).to have_http_status(:ok)
         end
       end
     end
@@ -30,32 +27,31 @@ describe BooksController do
       before do
         get :index
       end
-      it 'responds with 401 status'do
+      it 'responds with 401 status' do
         expect(response).to have_http_status(401)
       end
     end
-
   end
 
   describe 'GET #shows' do
     context 'with an authenticated user' do
       include_context 'Authenticated User'
-        context 'When fetching a book id' do
-          let!(:book) { FactoryBot.create(:book) }
+      context 'When fetching a book id' do
+        let!(:book) { FactoryBot.create(:book) }
 
-          before do
-            get :shows, params: { id: book.id }
-          end
+        before do
+          get :shows, params: { id: book.id }
+        end
 
-          it 'responses with the book id json' do
-            expect(response.body.to_json) =~ BookSerializer.new(
-              book, root: false
-            ).to_json
-          end
+        it 'responses with the book id json' do
+          expect(response.body.to_json) =~ BookSerializer.new(
+            book, root: false
+          ).to_json
+        end
 
-          it 'responds with 200 status' do
-            expect(response).to have_http_status(:ok)
-          end
+        it 'responds with 200 status' do
+          expect(response).to have_http_status(:ok)
+        end
       end
     end
 
@@ -63,13 +59,9 @@ describe BooksController do
       before do
         get :shows
       end
-      it 'responds with 401 status'do
+      it 'responds with 401 status' do
         expect(response).to have_http_status(401)
       end
     end
-
-
-
   end
-
 end
