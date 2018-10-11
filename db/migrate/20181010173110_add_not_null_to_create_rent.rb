fac471f22063a3a24289eb83d983f51b6a7be467
Ignacio <ignacio.oliveto@wolox.com.ar>
2018-10-11 14:01:16 -0300

    Fixing bad pr

--- db/migrate/20181010173110_add_not_null_to_create_rent.rb
+++ db/migrate/20181010173110_add_not_null_to_create_rent.rb
@@ -1,9 +1,6 @@
 class AddNotNullToCreateRent < ActiveRecord::Migration[5.1]
   def change
-
-      
       change_column_null(:rents, :rent_date, false)
       change_column_null(:rents, :rent_end, false)
-
   end
 end
