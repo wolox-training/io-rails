b80ebef678ed12499808676af3aba4be91e1aabd
Ignacio <ignacio.oliveto@wolox.com.ar>
2018-10-11 14:52:58 -0300

    welcome controller deleted

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,5 @@
 class ApplicationController < ActionController::Base
+  include Wor::Paginate
+  include DeviseTokenAuth::Concerns::SetUserByToken
   protect_from_forgery with: :null_session
 end
--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,4 @@
 class BooksController < ApplicationController
-  include Wor::Paginate
-  include DeviseTokenAuth::Concerns::SetUserByToken
   before_action :authenticate_user!
   def index
     render_paginated Book.all
