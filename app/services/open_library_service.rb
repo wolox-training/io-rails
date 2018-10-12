95c5b5318b6d771ec96919a554132b9829700d0c
Ignacio <ignacio.oliveto@wolox.com.ar>
2018-10-12 20:06:34 -0300

    Authors maped

--- app/services/open_library_service.rb
+++ app/services/open_library_service.rb
@@ -8,7 +8,7 @@ class OpenLibraryService
     response = HTTParty.get("https://openlibrary.org/api/books?bibkeys=ISBN:#{isbn}&format=json&jscmd=data")
     parsed = response.parsed_response
     parsed_r = ParsedResponse.new(isbn,parsed)
-    parsed_r
+    parsed_r #devuelve el objeto
   end
 
 end
@@ -18,7 +18,8 @@ class ParsedResponse
   @isbn = isbn
   @title = generate_title(parsed)
   @subtitle = generate_subtitle(parsed)
-  
+  @authors = generate_authors(parsed)
+
   end
 
   def generate_title(parsed)
@@ -27,5 +28,9 @@ class ParsedResponse
 
   def generate_subtitle(parsed)
     parsed["ISBN:#{@isbn}"]["subtitle"]
+  end
+
+  def generate_authors(parsed)
+    parsed["ISBN:#{@isbn}"]["authors"].map{|author| author["name"]}
   end
 end
