1856240b6fd953761612949e822df02feb58bf3b
Ignacio <ignacio.oliveto@wolox.com.ar>
2018-10-17 14:38:18 -0300

    Fixes requested in pr aded (better routes with resources and queries removed)

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
-  get 'welcome/index'
 
   mount_devise_token_auth_for 'User', at: 'auth'
   resources :books, only: [:index, :show]
