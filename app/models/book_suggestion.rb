58c5eaec325ec9365a1b9c49eb6215aa8126a4b0
Ignacio <ignacio.oliveto@wolox.com.ar>
2018-10-18 16:06:58 -0300

    Validate no less than 0 price

--- app/models/book_suggestion.rb
+++ app/models/book_suggestion.rb
@@ -1,4 +1,5 @@
 class BookSuggestion < ApplicationRecord
   belongs_to :user, optional: true
   validates :title, :author, :synopsis, :year, :publisher, presence: true
+  validates :price , numericality: {greater_than: 0}
 end
