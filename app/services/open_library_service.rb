b4a67f9f2ceebabaaa9373e12ffeae19d536a609
Ignacio <ignacio.oliveto@wolox.com.ar>
2018-10-19 14:30:07 -0300

    Correct url for external api added

--- app/services/open_library_service.rb
+++ app/services/open_library_service.rb
@@ -1,7 +1,8 @@
 class OpenLibraryService
   def book_info(param)
     response = HTTParty.get(
-      "#{Rails.application.secrets[:url_ext_api]}#{param}&format=json&jscmd=data"
+      "#{Rails.application.secrets[:url_ext_api]}
+      /books?bibkeys=ISBN:#{param}&format=json&jscmd=data"
     )
     ParsedResponse.new(param, response.parsed_response).as_json
   end
