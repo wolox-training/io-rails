74cc5a3fc36f945c7bb9c9294ae4f03d35be5b37
Ignacio <ignacio.oliveto@wolox.com.ar>
2018-10-19 20:00:26 -0300

    rubocop offences corrected

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,5 @@
 class ApplicationController < ActionController::Base
   include Wor::Paginate
   protect_from_forgery with: :null_session
-<<<<<<< HEAD
   include Pundit
-=======
->>>>>>> 057373f... Fixed  rubocop offences
 end
--- app/mailers/rent_mailer.rb
+++ app/mailers/rent_mailer.rb
@@ -6,4 +6,19 @@ class RentMailer < ApplicationMailer
     mail(to: @user.email,
          subject: 'Book Rent')
   end
+
+
+  def rent_remember(rent)
+    @user = User.find(rent.user_id)
+    @rent = rent
+    @book = Book.find(rent.book_id)
+    mail(to: @user.email, subject: 'Book Remember')
+  end
+
+  def rent_expiration(rent)
+    @user = User.find(rent.user_id)
+    @rent = rent
+    @book = Book.find(rent.book_id)
+    mail(to: @user.email, subject: 'Book Expiration')
+  end
 end
