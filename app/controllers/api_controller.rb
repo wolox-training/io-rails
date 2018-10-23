2c4cc99ef0e9ee4bd1e204541f582042c8a04966
Ignacio <ignacio.oliveto@wolox.com.ar>
2018-10-23 15:44:44 -0300

    Rspec problems fixed

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,7 +1,7 @@
 class ApiController < ApplicationController
   protect_from_forgery with: :null_session
   include DeviseTokenAuth::Concerns::SetUserByToken
-  # before_action :authenticate_user!
+  before_action :authenticate_user!
   rescue_from ActionController::ParameterMissing, with: :show_errors
 
   def show_errors
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,4 @@ class ApplicationController < ActionController::Base
   include Wor::Paginate
   include Pundit
   protect_from_forgery with: :null_session
-  #  rescue_from ActionController::ParameterMissing, with: :show_errors
-
-  # def show_errors
-  # render json: "ISBN can't be blank"
-  #  end
 end
